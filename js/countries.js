40043ead3e3b75c9b3c8337514343b8a2f78ca18
tanverrana <tanversobur@gmail.com>
2022-03-15 04:17:37 +0600

    fetch

---
+++ js/countries.js
@@ -0,0 +1,26 @@
+const loadCountries = () => {
+    fetch("https://restcountries.com/v3.1/all")
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+
+}
+
+const displayCountries = countries => {
+    console.log(countries);
+    const countriesHTML = countries.map(country => getCountryHtml(country));
+    //console.log(countriesHTML[0]);
+    const container = document.getElementById("countries");
+    container.innerHTML = countriesHTML.join(" ");
+}
+
+/* const getCountryHtml = country => {
+    //option-1
+    const { name, flags } = country;
+    return `
+    <div class="country">
+        <h2>${name.common}</h2>
+        <img src="${flags.png}">
+    </div>
+    `
+} */
+loadCountries();
